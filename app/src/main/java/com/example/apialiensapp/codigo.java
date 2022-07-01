7e7a170491845b8eee375d29f7d8e3128ce03a3e
Alfred-in <alfredo_1319104653@uptecamac.edu.mx>
2022-06-30 23:07:27 -0500

    activities

---
+++ app/src/main/java/com/example/apialiensapp/codigo.java
@@ -0,0 +1,14 @@
+package com.example.apialiensapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class codigo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_codigo);
+    }
+}
